49b2e282c019f2740a0dabb66be53d99f5e268c6
Michael Burgess <michael@Michaels-Laptop.local>
2023-11-25 08:53:13 +0000

    3rd

--- deploy.sh
+++ deploy.sh
@@ -21,38 +21,40 @@ if [ $# -ne 1 ]; then
 fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    brew update && brew install git curl vim wget gcc libsecret make nginx gh
-    sudo mkdir /opt/flip && chown "$USER:staff" /opt/flip
-    sudo mkdir /opt/flip/www && chown "$USER:staff" /opt/flip/www
-    sudo mkdir /opt/flip/www/home && chown "$USER:staff" /opt/flip/www/home
+    brew update && brew install git curl vim wget llvm libsecret make nginx gh openssl
+    sudo mkdir /opt/flip && sudo chown "$USER:admin" /opt/flip
+    sudo mkdir /opt/flip/www && sudo chown "$USER:admin" /opt/flip/www
+    sudo mkdir /opt/flip/www/home && sudo chown "$USER:admin" /opt/flip/www/home
     cd /opt/flip && git clone https://github.com/michaelburgess-d/flip-www-config.git config
-    cd /opt/flip/www && git clone https://github.com/michaelburgess-d/flip-ide-macos.git ide
+    cd /opt/flip/www && git clone https://github.com/michaelburgess-d/flip-ide-macos-arm.git ide
     cd /opt/flip/www && git clone https://github.com/michaelburgess-d/flip-www-public.git flipbook
+    
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /opt/flip/localhost.key -out /opt/flip/localhost.crt
+    sudo cp /opt/flip/config/flip-nginx.config /opt/homebrew/etc/nginx/servers/flip-nginx.config
+    sudo sed -i "s/__PASSWORD__/$1/g" /opt/homebrew/etc/nginx/servers/flip-nginx.config
 
-    cp /opt/flip/config/flip-nginx.config /opt/homebrew/etc/nginx/servers/
-    sed -i "s/__PASSWORD__/$1/g" /opt/homebrew/etc/nginx/servers/flip-nginx.config
 else
-    sudo apt update && sudo apt install -y git curl vim wget gcc g++ pkg-config libsecret-1-dev make nginx gh
-    sudo mkdir /opt/flip && chown "$USER:$USER" /opt/flip
-    sudo mkdir /opt/flip/www && chown "www-data:www-data" /opt/flip/www
-    sudo mkdir /opt/flip/www/home && chown "$USER:staff" /opt/flip/www/home
+    sudo apt update && sudo apt install -y git curl vim wget gcc g++ pkg-config libsecret-1-dev make nginx gh openssl
+    sudo mkdir /opt/flip && sudo chown "$USER:$USER" /opt/flip
+    sudo mkdir /opt/flip/www && sudo chown "www-data:www-data" /opt/flip/www
+    sudo mkdir /opt/flip/www/home && sudo chown "$USER:staff" /opt/flip/www/home
     cd /opt/flip && git clone https://github.com/michaelburgess-d/flip-www-config.git config
     cd /opt/flip/www && git clone https://github.com/michaelburgess-d/flip-ide-ubuntu-aws.git ide
     cd /opt/flip/www && git clone https://github.com/michaelburgess-d/flip-www-public.git flipbook
     
-    cp /opt/flip/config/flip-nginx.config /etc/nginx/sites-enabled/default
-    sed -i "s/__PASSWORD__/$1/g" /etc/nginx/sites-enabled/default
+    sudo cp /opt/flip/config/flip-nginx.config /etc/nginx/sites-enabled/default
+    sudo sed -i "s/__PASSWORD__/$1/g" /etc/nginx/sites-enabled/default
 fi
 
 
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-
 source "$HOME/.nvm/nvm.sh"
 nvm install v19.1.0
 npm install -g yarn
 
-cd /opt/flip/www/ide && nvm use v19.1.0 && yarn theia start --hostname 0.0.0.0 --port 3000
+echo "\n ----- DONE ----- \n"
+echo "\n -----      ----- \n"
+echo "run this script again to turn everything on"
 
 # cd /opt/www/ide && yarn
 # cd /opt/www/ide && yarn theia build
--- run.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update && sudo apt install -y git curl vim wget nginx gh
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-sudo mkdir /svr/www && chown www-data:www-data /srv/www
-cd /srv/www && git clone https://github.com/michaelburgess-d/ide.git 
-cd /srv/www && git clone https://github.com/michaelburgess-d/flip.git 
-bash ~/.bashrc && ~/.nvm/nvm.sh 
-nvm install v19.1.0 && npm install -g yarn  
-cd /srv/www/ide && nvm use v19.1.0 && yarn theia start --hostname 0.0.0.0
